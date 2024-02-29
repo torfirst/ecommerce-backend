dfac7ac462e98208468947595b9531b9d7d29858
tor first <victoriah.first@gmail.com>
2024-02-28 23:05:32 -0600

    removed comments from tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,8 +4,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
   try {
     const tags = await Tag.findAll({
       include: [
@@ -22,8 +20,6 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try{
     const tag = await Tag.findOne({
       include: [Product],
@@ -38,7 +34,6 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new tag
   try{
     const tag = await Tag.create(req.body)
     res.json(tag)
@@ -48,7 +43,6 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
   try{
     const tag = await Tag.update(req.body, {
       where: {
@@ -62,7 +56,6 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
   try{
     const tag = await Tag.destroy({
       where: {

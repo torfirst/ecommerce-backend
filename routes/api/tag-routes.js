a77ab2ad37ca34cd05a0b75d3fa61de2de6abf60
tor first <victoriah.first@gmail.com>
2024-02-28 23:04:17 -0600

    fixed error

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -50,7 +50,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try{
-    const tag = await EventTarget.update(req.body, {
+    const tag = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       }
